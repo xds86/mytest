66b414120ee2c25808950f11e03e423f9c73660f
xds86 <xds_86@live.com>
2020-10-26 15:54:43 +0800

    添加了第三个的输出

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,5 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("dis");
         System.out.println("第二行");
+        System.out.println("第三行");
+
     }
 }
